8c57c47c210f7353ce9d68db188384ea25493c2b
Algorithman <algorithman.chris@googlemail.com>
2014-02-15 10:17:13 +0100

    Initial commit

---
+++ AO-Tooler/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AO_Tooler
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
